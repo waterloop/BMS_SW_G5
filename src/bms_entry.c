9bc111198ccdbf662644458cdb3f5bb8316b8aa6
RyanJMah <rmah.uwaterloo.ca>
2021-05-02 12:44:35 -0600

    got uart working with high speed clock

--- src/bms_entry.c
+++ src/bms_entry.c
@@ -26,18 +26,20 @@ void blinky_loop() {
 }
 
 int bms_entry() {
-	blinky_loop();
+	// blinky_loop();
 	// slave_device = Ltc6813_init();
-	// GPIOB->MODER &= ~(0b11u << (10*2));
-	// GPIOB->MODER |= (0b01u << (10*2));
+	GPIOB->MODER &= ~(0b11u << (10*2));
+	GPIOB->MODER |= (0b01u << (10*2));
 
-	// unsigned char str[1] = {0x69};
-	// while (1) {
-	// 	print(str, 1);
-	// 	// GPIOB->ODR ^= (1 << 10);
-	// 	// bad_delay();
-	// 	// Ltc6813_write_spi(&slave_device, )
-	// }
+	
+	unsigned char str[15] = "I hate you!!!!\n\0";
+	
+	while (1) {
+		print(str, 15);
+		GPIOB->ODR ^= (1 << 10);
+		bad_delay();
+		// Ltc6813_write_spi(&slave_device, )
+	}
 
 	return 0;
 }
