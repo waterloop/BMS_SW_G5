55e2f71d44406120409df7447fd31a82a18719bd
RyanJMah <rmah.uwaterloo.ca>
2021-06-13 17:49:56 -0600

    merge ltc6813_driver_dev and master

--- STM32F405RGTx/Core/Src/measurements_thread.c
+++ STM32F405RGTx/Core/Src/measurements_thread.c
@@ -14,40 +14,17 @@ const osThreadAttr_t measurements_thread_attrs = {
 };
 
 void measurements_thread_fn(void* arg) {
+<<<<<<< HEAD
 	Ltc6813 slave_device = Ltc6813_init(hspi1, GPIOB, 12);
 
 	Ltc6813_wakeup_sleep(&slave_device);
 	uint8_t success;
 	HAL_Delay(1000);
 
+=======
+>>>>>>> merge ltc6813_driver_dev and master
 	while (1) {
-		Ltc6813_wakeup_sleep(&slave_device);
-
-		uart1_print("CFG A");
-
-		success = Ltc6813_read_cfga(&slave_device);
-
-		if (success) {
-			uart1_print("PEC SUCCESS");
-		} else {
-			uart1_print("PEC FAIL");
-		}
-
-		Buffer_print(&(slave_device.cfga_bfr));
-
-		uart1_print("CFG B");
-
-		success = Ltc6813_read_cfgb(&slave_device);
-
-		if (success) {
-			uart1_print("PEC SUCCESS");
-		} else {
-			uart1_print("PEC FAIL");
-		}
-
-		Buffer_print(&(slave_device.cfgb_bfr));
-
-		HAL_Delay(500);
+		asm("NOP");
 	}
 }
 
