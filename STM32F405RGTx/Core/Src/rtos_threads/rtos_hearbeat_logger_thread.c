6d6e135ac267fb3e624686b50e806da97bf432d9
Alfonso-Faria <alfonso.e@waterloop.ca>
2021-06-17 15:51:05 -0400

    we got blinky working

--- STM32F405RGTx/Core/Src/rtos_threads/rtos_hearbeat_logger_thread.c
+++ STM32F405RGTx/Core/Src/rtos_threads/rtos_hearbeat_logger_thread.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
+#include <stdint.h>
 #include "cmsis_os.h"
+
+#include "main.h"
+#include "peripherals.h"
 #include "threads.h"
 
 const osThreadAttr_t rtos_heartbeat_logger_thread_attrs = {
@@ -9,7 +12,7 @@ const osThreadAttr_t rtos_heartbeat_logger_thread_attrs = {
 };
 void rtos_heartbeat_logger_thread(void* arg) {
 	while (1) {
-		printf("rtos is still running...");
+		printf("rtos is still running... \r\n");
 		osDelay(10000);
 	}
 }
