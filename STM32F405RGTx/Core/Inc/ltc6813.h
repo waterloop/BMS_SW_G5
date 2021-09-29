e41d18429a470308b80f4fe745387b851fef9c37
Ryan Mah <ryan.m@waterloop.ca>
2021-09-28 22:13:18 -0600

    merging ltc6813 drivers

--- STM32F405RGTx/Core/Inc/ltc6813.h
+++ STM32F405RGTx/Core/Inc/ltc6813.h
@@ -5,7 +5,7 @@
 #include "main.h"
 
 #ifndef LTC6813_BUFFER_SIZE
-#define LTC6813_BUFFER_SIZE 	100 // bytes
+#define LTC6813_BUFFER_SIZE 	10 // bytes
 #endif
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -41,6 +41,13 @@ typedef struct {
 	Buffer cfga_bfr;
 	Buffer cfgb_bfr;
 
+	Buffer cva_bfr;
+	Buffer cvb_bfr;
+	Buffer cvc_bfr;
+	Buffer cvd_bfr;
+	Buffer cve_bfr;;
+	Buffer cvf_bfr;
+
 	uint32_t timeout;
 } Ltc6813;
 
@@ -54,28 +61,69 @@ void Ltc6813_cs_high(Ltc6813* self);
 void Ltc6813_wakeup_sleep(Ltc6813* self);
 void Ltc6813_wakeup_idle(Ltc6813* self);
 
+void Ltc6813_print_voltages(Ltc6813* self);
+
 void Ltc6813_write_spi(Ltc6813* self, Buffer* buffer);
 void Ltc6813_read_spi(Ltc6813* self, Buffer* buffer);
 //void Ltc6813_write_read_spi(Ltc6813* self, Buffer* buffer);
 
 void Ltc6813_send_cmd(Ltc6813* self, uint16_t cmd);
 
+uint8_t Ltc6813_read_reg(Ltc6813* self, uint8_t reg_cmd);
+void Ltc6813_write_reg(Ltc6813* self, uint8_t reg_cmd);
+
 uint8_t Ltc6813_read_cfga(Ltc6813* self);
 uint8_t Ltc6813_read_cfgb(Ltc6813* self);
+void Ltc6813_write_cfga(Ltc6813* self);
+void Ltc6813_write_cfgb(Ltc6813* self);
+
+uint8_t Ltc6813_read_adc(Ltc6813* self, uint16_t mode);
+
+/*
+cell_mask is a mask of 32 bits where the i-th bit represents whether or not a cell should be discharged
+	- 1 -> balance cell, 0 -> don't balance cell.
+	- only the first 18 LSBs are used, the remaining 14 are don't cares
+
+ex. cell_mask = 0bXXXXXXXXXXXXXX000010000100001001
+		==> cells 0, 3, 8, and 13 are being discharged, the rest are not
+*/
+uint8_t Ltc6813_discharge_ctrl(Ltc6813* self, uint32_t cell_mask);
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 // Ltc6813 Command Code Defines
-
- #ifndef __LTC6813_COMMAND_CODES
+#ifndef __LTC6813_COMMAND_CODES
 
 #define WRCFGA 		0b00000000001u
 #define WRCFGB 		0b00000100100u
 
- #define RDCFGA		0b00000000010u
- #define RDCFGB		0b00000100110u
+#define RDCFGA		0b00000000010u
+#define RDCFGB		0b00000100110u
 
- #endif
+#define RDCVA	0b00000000100u
+#define RDCVB	0b00000000110u
+#define RDCVC	0b00000001000u
+#define RDCVD	0b00000001010u
+#define RDCVE	0b00000001001u
+#define RDCVF	0b00000001011u
+
+#define PLADC 	0b11100010100u
+
+#endif
+
+
+#ifndef __LTC6813_ADC_MODES
+
+#define FAST_ADC 			0b01011100000u
+#define FAST_ADC_DELAY		2
+
+#define NORMAL_ADC 			0b01101100000u
+#define NORMAL_ADC_DELAY	3
+
+#define FILTERED_ADC 		0b01111100000u
+#define FILTERED_ADC_DELAY	202
+
+#endif
 
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
--- STM32F405RGTx/Core/Src/ltc6813.c
+++ STM32F405RGTx/Core/Src/ltc6813.c
@@ -4,7 +4,6 @@
 
 #include "ltc6813.h"
 #include "timer_utils.h"
-#include "lut.h"
 
 /*
 IMPORTANT TIMING PARAMETERS:
@@ -21,6 +20,32 @@ IMPORTANT TIMING PARAMETERS:
 		- t_idle = 4.3ms (min) to 6.7ms (max)
 			- time for the chip's isoSPI tranceiver to transition into its IDLE state
 */
+
+const uint16_t _CRC15_LUT[256] = {
+	0x0,0xc599, 0xceab, 0xb32, 0xd8cf, 0x1d56, 0x1664, 0xd3fd, 0xf407, 0x319e, 0x3aac,
+	0xff35, 0x2cc8, 0xe951, 0xe263, 0x27fa, 0xad97, 0x680e, 0x633c, 0xa6a5, 0x7558, 0xb0c1,
+	0xbbf3, 0x7e6a, 0x5990, 0x9c09, 0x973b, 0x52a2, 0x815f, 0x44c6, 0x4ff4, 0x8a6d, 0x5b2e,
+	0x9eb7, 0x9585, 0x501c, 0x83e1, 0x4678, 0x4d4a, 0x88d3, 0xaf29, 0x6ab0, 0x6182, 0xa41b,
+	0x77e6, 0xb27f, 0xb94d, 0x7cd4, 0xf6b9, 0x3320, 0x3812, 0xfd8b, 0x2e76, 0xebef, 0xe0dd,
+	0x2544, 0x2be, 0xc727, 0xcc15, 0x98c, 0xda71, 0x1fe8, 0x14da, 0xd143, 0xf3c5, 0x365c,
+	0x3d6e, 0xf8f7,0x2b0a, 0xee93, 0xe5a1, 0x2038, 0x7c2, 0xc25b, 0xc969, 0xcf0, 0xdf0d,
+	0x1a94, 0x11a6, 0xd43f, 0x5e52, 0x9bcb, 0x90f9, 0x5560, 0x869d, 0x4304, 0x4836, 0x8daf,
+	0xaa55, 0x6fcc, 0x64fe, 0xa167, 0x729a, 0xb703, 0xbc31, 0x79a8, 0xa8eb, 0x6d72, 0x6640,
+	0xa3d9, 0x7024, 0xb5bd, 0xbe8f, 0x7b16, 0x5cec, 0x9975, 0x9247, 0x57de, 0x8423, 0x41ba,
+	0x4a88, 0x8f11, 0x57c, 0xc0e5, 0xcbd7, 0xe4e, 0xddb3, 0x182a, 0x1318, 0xd681, 0xf17b,
+	0x34e2, 0x3fd0, 0xfa49, 0x29b4, 0xec2d, 0xe71f, 0x2286, 0xa213, 0x678a, 0x6cb8, 0xa921,
+	0x7adc, 0xbf45, 0xb477, 0x71ee, 0x5614, 0x938d, 0x98bf, 0x5d26, 0x8edb, 0x4b42, 0x4070,
+	0x85e9, 0xf84, 0xca1d, 0xc12f, 0x4b6, 0xd74b, 0x12d2, 0x19e0, 0xdc79, 0xfb83, 0x3e1a, 0x3528,
+	0xf0b1, 0x234c, 0xe6d5, 0xede7, 0x287e, 0xf93d, 0x3ca4, 0x3796, 0xf20f, 0x21f2, 0xe46b, 0xef59,
+	0x2ac0, 0xd3a, 0xc8a3, 0xc391, 0x608, 0xd5f5, 0x106c, 0x1b5e, 0xdec7, 0x54aa, 0x9133, 0x9a01,
+	0x5f98, 0x8c65, 0x49fc, 0x42ce, 0x8757, 0xa0ad, 0x6534, 0x6e06, 0xab9f, 0x7862, 0xbdfb, 0xb6c9,
+	0x7350, 0x51d6, 0x944f, 0x9f7d, 0x5ae4, 0x8919, 0x4c80, 0x47b2, 0x822b, 0xa5d1, 0x6048, 0x6b7a,
+	0xaee3, 0x7d1e, 0xb887, 0xb3b5, 0x762c, 0xfc41, 0x39d8, 0x32ea, 0xf773, 0x248e, 0xe117, 0xea25,
+	0x2fbc, 0x846, 0xcddf, 0xc6ed, 0x374, 0xd089, 0x1510, 0x1e22, 0xdbbb, 0xaf8, 0xcf61, 0xc453,
+	0x1ca, 0xd237, 0x17ae, 0x1c9c, 0xd905, 0xfeff, 0x3b66, 0x3054, 0xf5cd, 0x2630, 0xe3a9, 0xe89b,
+	0x2d02, 0xa76f, 0x62f6, 0x69c4, 0xac5d, 0x7fa0, 0xba39, 0xb10b, 0x7492, 0x5368, 0x96f1, 0x9dc3,
+	0x585a, 0x8ba7, 0x4e3e, 0x450c, 0x8095
+};
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////
 // Buffer methods
@@ -50,7 +75,7 @@ void Buffer_add_pec(Buffer* self) {
 	uint16_t addr = 0;
 	for (uint8_t i = 0; i < self->len; i++) {
 		addr = ( (remainder >> 7)^Buffer_index(self, i) ) & 0xff;
-		remainder = (remainder << 8)^CRC15_LUT[addr];
+		remainder = (remainder << 8)^_CRC15_LUT[addr];
 	}
 	uint16_t pec = remainder*2;
 	Buffer_append(self, (pec >> 8) & 0xff);
@@ -62,7 +87,7 @@ uint8_t Buffer_check_pec(Buffer* self) {
 	uint16_t addr = 0;
 	for (uint8_t i = 0; i < self->len - 2; i++) {
 		addr = ( (remainder >> 7)^Buffer_index(self, i) ) & 0xff;
-		remainder = (remainder << 8)^CRC15_LUT[addr];
+		remainder = (remainder << 8)^_CRC15_LUT[addr];
 	}
 	uint16_t calc_pec = remainder*2;
 	uint16_t act_pec = ((uint16_t)Buffer_index(self, self->len - 2) << 8) | Buffer_index(self, self->len - 1);
@@ -104,6 +129,13 @@ Ltc6813 Ltc6813_init(SPI_HandleTypeDef spi, GPIO_TypeDef* cs_gpio_port, uint8_t 
 	slave_device.cfga_bfr = Buffer_init();
 	slave_device.cfgb_bfr = Buffer_init();
 
+	slave_device.cva_bfr = Buffer_init();
+	slave_device.cvb_bfr = Buffer_init();
+	slave_device.cvc_bfr = Buffer_init();
+	slave_device.cvd_bfr = Buffer_init();
+	slave_device.cve_bfr = Buffer_init();
+	slave_device.cvf_bfr = Buffer_init();
+
 	slave_device.timeout = 10000;
 
 	Ltc6813_cs_high(&slave_device);
@@ -125,7 +157,30 @@ void Ltc6813_wakeup_sleep(Ltc6813* self) {
 void Ltc6813_wakeup_idle(Ltc6813* self) {
 	Ltc6813_cs_low(self);
 	delay_us(20);		// according to datasheet, t_wake = 10us
+	HAL_SPI_Receive(&self->_spi_interface, 0xff, 1, self->timeout);
 	Ltc6813_cs_high(self);
+}
+
+void Ltc6813_print_voltages(Ltc6813* self) {
+
+	printf("PRINTING CVA\r\n");
+	Buffer_print(&(self->cva_bfr));
+
+	printf("PRINTING CVB\r\n");
+	Buffer_print(&(self->cvb_bfr));
+
+	printf("PRINTING CVC\r\n");
+	Buffer_print(&(self->cvc_bfr));
+
+	printf("PRINTING CVD\r\n");
+	Buffer_print(&(self->cvd_bfr));
+
+	printf("PRINTING CVE\r\n");
+	Buffer_print(&(self->cve_bfr));
+
+	printf("PRINTING CVF\r\n");
+	Buffer_print(&(self->cvf_bfr));
+
 }
 
 // READ COMMAND FUNCTIONS:
@@ -141,39 +196,149 @@ void Ltc6813_send_cmd(Ltc6813* self, uint16_t cmd) {
 	HAL_SPI_Transmit(&self->_spi_interface, self->cmd_bfr.data, self->cmd_bfr.len, self->timeout);
 }
 
-uint8_t Ltc6813_read_cfga(Ltc6813* self) {
-	Buffer_clear(&self->cfga_bfr);
+uint8_t Ltc6813_read_reg(Ltc6813* self, uint8_t reg_cmd) {
+	Buffer* reg_buf;
 
-	self->cfga_bfr.len = 8;
+	switch (reg_cmd) {
+		case RDCFGA:
+			reg_buf = &(self->cfga_bfr);
+			break;
+		case RDCFGB:
+			reg_buf = &(self->cfgb_bfr);
+			break;
+		case RDCVA:
+			reg_buf = &(self->cva_bfr);
+			break;
+		case RDCVB:
+			reg_buf = &(self->cvb_bfr);
+			break;
+		case RDCVC:
+			reg_buf = &(self->cvc_bfr);
+			break;
+		case RDCVD:
+			reg_buf = &(self->cvd_bfr);
+			break;
+		case RDCVE:
+			reg_buf = &(self->cve_bfr);
+			break;
+		case RDCVF:
+			reg_buf = &(self->cvf_bfr);
+			break;
+		default:
+			break;
+	}
+
+	Buffer_clear(reg_buf);
+
+	reg_buf->len = 8;
 
 	Ltc6813_cs_low(self);
 
-	Ltc6813_send_cmd(self, RDCFGA);
-	HAL_SPI_Receive(&self->_spi_interface, self->cfga_bfr.data, self->cfga_bfr.len, self->timeout);
+	Ltc6813_send_cmd(self, reg_cmd);
+	HAL_SPI_Receive(&self->_spi_interface, reg_buf->data, reg_buf->len, self->timeout);
 
 	Ltc6813_cs_high(self);
 
-	uint8_t pec_success = Buffer_check_pec(&self->cfga_bfr);
-	self->cfga_bfr.len = 6;
+	uint8_t pec_success = Buffer_check_pec(reg_buf);
+	reg_buf->len = 6;
 
 	return pec_success;
+
+}
+void Ltc6813_write_reg(Ltc6813* self, uint8_t reg_cmd) {
+	Buffer* reg_buff;
+	if (reg_cmd == WRCFGA) {
+		reg_buff = &self->cfga_bfr;
+	}
+	else if (reg_cmd == WRCFGB) {
+		reg_buff = &self->cfgb_bfr;
+	}
+	else {
+		printf("Error: command code %d is not implemented yet...", reg_cmd);
+		Error_Handler();
+	}
+	Buffer_add_pec(reg_buff);
+	Ltc6813_cs_low(self);
+	Ltc6813_send_cmd(self, reg_cmd);
+	HAL_SPI_Transmit(&self->_spi_interface, reg_buff->data, reg_buff->len, self->timeout);
+	Ltc6813_cs_high(self);
+	reg_buff->len = 6;
 }
 
-uint8_t Ltc6813_read_cfgb(Ltc6813* self) {
-	Buffer_clear(&self->cfgb_bfr);
+uint8_t Ltc6813_read_cfga(Ltc6813* self) { return Ltc6813_read_reg(self, RDCFGA); }
+uint8_t Ltc6813_read_cfgb(Ltc6813* self) { return Ltc6813_read_reg(self, RDCFGB); }
+void Ltc6813_write_cfga(Ltc6813* self) { return Ltc6813_write_reg(self, WRCFGA); }
+void Ltc6813_write_cfgb(Ltc6813* self) { return Ltc6813_write_reg(self, WRCFGB); }
 
-	self->cfgb_bfr.len = 8;
-
+uint8_t Ltc6813_read_adc(Ltc6813* self, uint16_t mode) {
 	Ltc6813_cs_low(self);
 
-	Ltc6813_send_cmd(self, RDCFGB);
-	HAL_SPI_Receive(&self->_spi_interface, self->cfgb_bfr.data, self->cfgb_bfr.len, self->timeout);
+	printf("REFERENCES POWERING UP\r\n");
+	Ltc6813_send_cmd(self, mode);
 
-	Ltc6813_cs_high(self);
+	// Wait for references to power up. Should be 4.4 ms, but can only delay integer ticks (1ms/tick)
+	osDelay(5);
 
-	uint8_t pec_success = Buffer_check_pec(&self->cfgb_bfr);
-	self->cfgb_bfr.len = 6;
+	printf("REFERENCES POWERED\r\n");
 
-	return pec_success;
+	uint32_t delay = FILTERED_ADC_DELAY;
+
+	if (mode == FAST_ADC) {
+		delay = FAST_ADC_DELAY;
+	} else if (mode == NORMAL_ADC) {
+		delay = NORMAL_ADC_DELAY;
+	} else if (mode == FILTERED_ADC) {
+		delay = FILTERED_ADC_DELAY;
+	}
+
+	osDelay(delay);
+
+	uint8_t success = 1;
+
+	success &= Ltc6813_read_reg(self, RDCVA);
+	printf("PEC CVA: %d\r\n", success);
+	success &= Ltc6813_read_reg(self, RDCVB);
+	printf("PEC CVB: %d\r\n", success);
+	success &= Ltc6813_read_reg(self, RDCVC);
+	printf("PEC CVC: %d\r\n", success);
+	success &= Ltc6813_read_reg(self, RDCVD);
+	printf("PEC CVD: %d\r\n", success);
+	success &= Ltc6813_read_reg(self, RDCVE);
+	printf("PEC CVE: %d\r\n", success);
+	success &= Ltc6813_read_reg(self, RDCVF);
+	printf("PEC CVF: %d\r\n", success);
+
+	return success;
+
+}
+
+uint8_t Ltc6813_discharge_ctrl(Ltc6813* self, uint32_t cell_mask) {
+	// CFGAR4 contains DCC[8:1]
+	uint8_t cfgar4 = cell_mask & 0b11111111U
+	Buffer_set_index(&self->cfga_bfr, 4U, cfgar4);
+
+	// CFGAR5 contains DCC[12:9] in the 4 LSBs
+	uint8_t cfgar5 = Buffer_index(&self->cfga_bfr, 5U);
+	cfgar5 &= ~(0b1111U);
+	cfgar5 |= (cell_mask >> 8U) & 0b1111U;
+	Buffer_set_index(&self->cfga_bfr, 5U, cfgar5);
+
+	// CFGBR0 contains DCC[16:13] in the 4 MSBs
+	uint8_t cfgbr0 = Buffer_index(&self->cfgb_bfr, 0U);
+	cfgbr0 &= ~(0b1111U << 4U);
+	cfgbr0 |= (cell_mask >> 12U) & 0b1111U;
+	Buffer_set_index(&self->cfgb_bfr, 0U, cfgbr0);
+
+	// CFGBR1 contains DCC[18:17] in the 2 LSBs
+	uint8_t cfgbr1 = Buffer_index(&self->cfgb_bfr, 1U);
+	cfgbr1 &= ~(0b11U);
+	cfgbr1 |= (cell_mask >> 16U) & 0b11U;
+	Buffer_set_index(&self->cfgb_bfr, 1U, cfgbr1);
+
+	// write back to the device
+	uint8_t status = 1U;
+	status &= Ltc6813_write_cfga(self);
+	status &= Ltc6813_write_cfgb(self);
+	return status;
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////
