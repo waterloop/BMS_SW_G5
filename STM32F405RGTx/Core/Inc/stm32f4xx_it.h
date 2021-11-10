1c2534735b432fe92af47d42265e331e9324e85c
Ryan Mah <rmah@uwaterloo.ca>
2021-11-09 18:06:09 -0700

    added rx interrupts on CAN

--- STM32F405RGTx/Core/Inc/stm32f4xx_it.h
+++ STM32F405RGTx/Core/Inc/stm32f4xx_it.h
@@ -53,6 +53,8 @@ void MemManage_Handler(void);
 void BusFault_Handler(void);
 void UsageFault_Handler(void);
 void DebugMon_Handler(void);
+void CAN1_RX0_IRQHandler(void);
+void CAN1_RX1_IRQHandler(void);
 void TIM1_BRK_TIM9_IRQHandler(void);
 void TIM3_IRQHandler(void);
 void DMA2_Stream0_IRQHandler(void);
