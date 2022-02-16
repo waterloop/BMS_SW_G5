f1c64ebf8e97b7bd3d148ed8db229857d0303612
Nushaab Syed <nushaab0202@gmail.com>
2022-02-16 16:17:33 -0500

    Initial commit of measurements_thread.hpp containing MeasurementsThread class. Changing syntax of fxns in .cpp file to conform as member classes

--- STM32F405RGTx/Core/Inc/measurements_thread.hpp
+++ STM32F405RGTx/Core/Inc/measurements_thread.hpp
@@ -1,1 +1,18 @@
+#pragma once
+#include "util.hpp"
 
+class MeasurementsThread {
+    public:
+        static void initialize();
+
+        static void _process_data();
+    private:
+        static RTOSThread thread;
+        static void runMeasurements(void* args);
+
+        static uint16_t ADC_buffer;
+        
+        static void _start_adc_and_dma();
+
+        static void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc);
+};
